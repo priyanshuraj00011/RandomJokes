38e9adc10ec96cecaf572d021ada7d8d7a803d18
Priyanshu <priyanshuraj00011@gmail.com>
2023-04-12 04:27:04 +0530

    We Build Random Jokes Application.

---
+++ src/main/java/com/geekster/randomJokesApplication/controller/jokesController.java
@@ -0,0 +1,28 @@
+package com.geekster.randomJokesApplication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class jokesController {
+    @GetMapping(value = "/jokes")
+    public String getJokes(){
+        ArrayList<String>jokesList=new ArrayList<>();
+        jokesList.add("I'm afraid for the calendar. Its days are numbered.");
+        jokesList.add("My wife said I should do lunges to stay in shape. That would be a big step forward.");
+        jokesList.add("Why do fathers take an extra pair of socks when they go golfing?");
+        jokesList.add("Singing in the shower is fun until you get soap in your mouth. Then it's a soap opera.");
+        jokesList.add("If April showers bring May flowers, what do May flowers bring?");
+        jokesList.add("I have a joke about chemistry, but I don't think it will get a reaction.");
+        jokesList.add("That car looks nice but the muffler seems exhausted.");
+        jokesList.add("It takes guts to be an organ donor.");
+        jokesList.add("I could tell a joke about pizza, but it's a little cheesy.");
+        jokesList.add("Don't trust atoms. They make up everything!");
+        jokesList.add("Is this pool safe for diving? It deep ends.");
+
+        int randomNumber = 0 + (int)(Math.random() *((10-0)+1));
+        return jokesList.get(randomNumber);
+    }
+}
---
+++ src/test/java/com/geekster/randomJokesApplication/RandomJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.randomJokesApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
